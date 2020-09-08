c30d412e9bec035c1fb7fa0db08d3e78c014cded
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-08 13:26:08 +0700

    perbaikan response cek nik

--- app/Http/Controllers/Register/CheckNikIsAlreadyUsedController.php
+++ app/Http/Controllers/Register/CheckNikIsAlreadyUsedController.php
@@ -4,17 +4,27 @@ namespace App\Http\Controllers\Register;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException;
 use Validator;
+use App\Entities\RdtApplicant;
 
 class CheckNikIsAlreadyUsedController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $validator = Validator::make($request->all(), ['nik' => 'required|unique:rdt_applicants']);
-        
+        $validator = Validator::make($request->all(), ['nik' => 'required']);
         if ($validator->fails()) {
-            throw ValidationException::withMessages(['nik' => 'NIK sudah pernah digunakan.']);
+            $statusCode = 422;
+            $response['message'] = $validator->messages();
+        } else {
+            $rdtApplicant = RdtApplicant::where('nik', $request->nik)->first();
+            if ($rdtApplicant!=null) {
+                $response['message'] = "nik sudah pernah digunakan";
+                $statusCode = 422;
+            } else {
+                $response['message'] = "nik belum pernah digunakan";
+                $statusCode = 200;
+            }
         }
+        return response()->json($response, $statusCode);
     }
 }
