66f853c5d1f25aa4e7fd5022d8da7619e1bf5a39
Nuris Akbar <nuris.akbar@gmail.com>
2020-09-09 11:13:50 +0700

    Create RdtInvitationExportExcelF1Controller.php

---
+++ app/Http/Controllers/Rdt/RdtInvitationExportExcelF1Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Rdt;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Exports\RdtInvitationExcel;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Entities\RdtEvent;
+
+class RdtInvitationExportExcelF1Controller extends Controller
+{
+    public function __invoke($id)
+    {
+        $rdtEvent = RdtEvent::findOrFail($id);
+        $fileName = str_replace(" ", "-", $rdtEvent->event_name);
+        return Excel::download(new RdtInvitationExcel($id), $fileName.'.xlsx');
+    }
+}
