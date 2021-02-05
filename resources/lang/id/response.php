a552d5ac5d9078a1258219dd898da04ca69abe66
Novansyah <rachadian.novansyah15@gmail.com>
2021-02-05 17:20:14 +0700

    Merapihkan spacy untuk response payload

--- resources/lang/id/response.php
+++ resources/lang/id/response.php
@@ -12,6 +12,6 @@ return [
     |
     */
 
-    'sync_failed' => 'gagal melakukan sinkronsiasi ke server tujuan',
-    'sync_success'=> 'data berhasil terkirim'
+    'sync_failed' => ' gagal melakukan sinkronsiasi ke server tujuan',
+    'sync_success' => ' data berhasil terkirim'
 ];
