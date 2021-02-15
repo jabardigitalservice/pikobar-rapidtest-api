7ed57a6748732403317c66b1e2d217fbdfc7f512
Novansyah <rachadian.novansyah15@gmail.com>
2021-02-15 17:22:14 +0700

    add validation when kode sample already exists or used

--- app/Http/Controllers/Rdt/RdtEventParticipantSetLabCodeController.php
+++ app/Http/Controllers/Rdt/RdtEventParticipantSetLabCodeController.php
@@ -6,6 +6,7 @@ use App\Entities\RdtInvitation;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
 
 class RdtEventParticipantSetLabCodeController extends Controller
 {
@@ -20,8 +21,16 @@ class RdtEventParticipantSetLabCodeController extends Controller
         /**
          * @var RdtInvitation $rdtInvitation
          */
-        $rdtInvitation                  = RdtInvitation::findOrFail($request->input('rdt_invitation_id'));
+        $rdtInvitation = RdtInvitation::findOrFail($request->input('rdt_invitation_id'));
+
+        // validasi if kode sample sudah digunakan
+        if (RdtInvitation::query()->where('lab_code_sample', $request->input('lab_code_sample'))->count() > 0) {
+            return response()->json([
+                'message' => 'Kode sample sudah ada atau sudah digunakan oleh peserta lain, harap memasukan kode sample dengan benar.',
+            ], HttpResponse::HTTP_BAD_REQUEST);
+        }
         $rdtInvitation->lab_code_sample = $request->input('lab_code_sample');
+
         if ($rdtInvitation->attended_at === null) {
             $rdtInvitation->attended_at = Carbon::now();
         }
