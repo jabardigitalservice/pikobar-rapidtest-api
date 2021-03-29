e88521cd1f6f7e19c7146980f35be9943f58a9ad
Novansyah <rachadian.novansyah15@gmail.com>
2021-03-29 16:31:43 +0700

    fixing bug city, village, district on detail applicant invitation

--- app/Http/Controllers/Rdt/RdtEventParticipantDetailController.php
+++ app/Http/Controllers/Rdt/RdtEventParticipantDetailController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers\Rdt;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Entities\RdtInvitation;
+use App\Http\Controllers\Controller;
 use App\Http\Resources\RdtInvitationResource;
 
 class RdtEventParticipantDetailController extends Controller
 {
     public function __invoke($id)
     {
-        $invitation = RdtInvitation::with('applicant')->findOrFail($id);
-        return new RdtInvitationResource($invitation);
+        $invitation = RdtInvitation::findOrFail($id);
+
+        // add lazy eager loading applicant to provide city, district and village
+        return new RdtInvitationResource($invitation->load([
+            'applicant', 'applicant.city', 'applicant.district', 'applicant.village',
+        ]));
     }
 }
