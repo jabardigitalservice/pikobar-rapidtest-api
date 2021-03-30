f4b04db78b19385dab8c9fa3d59337dfe3615bcb
Novansyah <rachadian.novansyah15@gmail.com>
2021-03-30 10:12:48 +0700

    change string null to null

--- app/Http/Controllers/Rdt/RdtInvitationResetController.php
+++ app/Http/Controllers/Rdt/RdtInvitationResetController.php
@@ -9,7 +9,7 @@ class RdtInvitationResetController extends Controller
 {
     public function __invoke(RdtInvitation $rdtInvitation)
     {
-        $rdtInvitation->update(['lab_code_sample' => 'null' ,'attended_at' => null,'attend_location' => null]);
+        $rdtInvitation->update(['lab_code_sample' => null ,'attended_at' => null,'attend_location' => null]);
         return response()->json(['message' => 'OK']);
     }
 }
