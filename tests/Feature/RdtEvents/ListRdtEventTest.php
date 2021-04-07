d00699e3f9d14919303a2660d3e8cf6df94e43c9
Novansyah <rachadian.novansyah15@gmail.com>
2021-04-08 06:34:58 +0700

    add case for per_page on list event test

--- tests/Feature/RdtEvents/ListRdtEventTest.php
+++ tests/Feature/RdtEvents/ListRdtEventTest.php
@@ -29,6 +29,28 @@ class ListRdtEventTest extends TestCase
     }
 
     /** @test */
+    public function can_show_all_rdt_events_with_per_page()
+    {
+        // 1. Mock the data
+        factory(RdtEvent::class, 50)->create();
+
+        $user = new User();
+        $user->assignPermissions(['list-events']);
+
+        $data = [
+            'per_page' => 15
+        ];
+
+        // 2. Hit endpoint
+        $response = $this->actingAs($user)->json('GET', '/api/rdt/events', $data);
+
+        // 3. Assertion
+        $response->assertSuccessful()
+            ->assertJsonStructure(['data', 'meta'])
+            ->assertJsonFragment((['per_page' => 15]));
+    }
+
+    /** @test */
     public function failed_to_show_events_because_unauthorized()
     {
         $response = $this->json('GET', '/api/rdt/events');
