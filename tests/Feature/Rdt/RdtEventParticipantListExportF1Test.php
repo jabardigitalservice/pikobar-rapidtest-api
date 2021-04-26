20cb944e2b883020152e019f9a9df0f5b02e19e6
Novansyah <rachadian.novansyah15@gmail.com>
2021-04-26 16:21:15 +0700

    test f1, f2 & survey

--- tests/Feature/Rdt/RdtEventParticipantListExportF1Test.php
+++ tests/Feature/Rdt/RdtEventParticipantListExportF1Test.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Response;
 use Maatwebsite\Excel\Facades\Excel;
 use Tests\TestCase;
 
-class RdtEventParticipantListExportF2Test extends TestCase
+class RdtEventParticipantListExportF1Test extends TestCase
 {
     /** @test */
     public function can_export_f2_applicant_event()
@@ -18,7 +18,7 @@ class RdtEventParticipantListExportF2Test extends TestCase
         $user = new User();
         $rdtEvent = factory(RdtEvent::class)->create();
 
-        $response = $this->actingAs($user)->get("/api/rdt/events/{$rdtEvent->id}/participants-export-f2");
+        $response = $this->actingAs($user)->get("/api/rdt/events/{$rdtEvent->id}/participants-export-f1");
 
         $response->assertStatus(Response::HTTP_OK)
             ->assertSuccessful();
